d6829bf9a8c251ace28f29056a033374e814cf17
Claire Wilbert <claire.wilbert@gmail.com>
2019-08-14 14:34:06 -0700

    start of seeds

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+require 'rest-client'
+require 'json'
+
+public_domain_url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
+
+individual_url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
+
+get_public_ids
