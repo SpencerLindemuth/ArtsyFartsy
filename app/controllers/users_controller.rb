10c8046d9917a39eebb68d0a3a9f4f3967a5e50d
Spencer Lindemuth <lindemut@gmail.com>
2019-08-20 14:05:12 -0700

    create user sends tokens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,13 @@ class UsersController < ApplicationController
         @user = User.find(params[:user_id])
         @user.pieces.destroy(params[:piece_id])
     end
+
+    def create
+        @user = User.new(params.permit(:username, :password))
+        if @user.save
+            render json: {id: @user.id, username: @user.username}
+        else
+            :bad_request
+        end
+    end
 end
