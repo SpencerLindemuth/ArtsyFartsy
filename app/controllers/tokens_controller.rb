0d6c2c866709fd2cdd386a6d05e8e29dd50212d8
Spencer Lindemuth <lindemut@gmail.com>
2019-08-20 15:27:41 -0700

    Login backend done

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,6 +1,6 @@
 class TokensController < ApplicationController
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.where("username ILIKE ?", params[:username]).first
     if @user.authenticate(params[:password])
       render json: {
         jwt: encode_token({id: @user.id, username: @user.username})
