11574e2d70eac8ae6acb245c5d4197b4d9800ec5
Spencer Lindemuth <lindemut@gmail.com>
2019-08-21 16:51:17 -0700

    change front-end url cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'artsy-fartsy-frontend.herokuapp.com'
+    origins 'artsyfartsy.herokuapp.com'
 
     resource '*',
       headers: :any,
